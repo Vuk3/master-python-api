ca8929ee83d342a285aa5af2cfdfa8d98e25ca03
Vuk Cvetković <vuk.cvetkovic@ncoded.solutions>
2026-01-23 18:56:28 +0100

    feat: add dummy function for predict

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return "OK from PYTHON"
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    if not file:
+        return JSONResponse(status_code=400, content="No file uploaded")
+
+    return {
+        "message": "Prediction result from PYTHON API",
+        "fileName": file.filename,
+        "contentType": file.content_type
+    }
+
 
 if __name__ == "__main__":
     import uvicorn
